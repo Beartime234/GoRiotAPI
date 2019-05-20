7a7cc94e0e185aa96d685f4bd18d8b303275f52d
Atsuya <themilkey@users.noreply.github.com>
2019-05-20 21:26:01 +0900

    update: wrap more API
    League-V4に対応しました。

---
+++ src/LeagueV4.go
@@ -0,0 +1,136 @@
+package riotapi
+
+import (
+	"encoding/json"
+)
+
+// LeagueListDTO represents the challenger league.
+type LeagueListDTO struct {
+	Tier     string `json:"tier"`
+	LeagueID string `json:"leagueId"`
+	Entries  []struct {
+		SummonerName string `json:"summonerName"`
+		HotStreak    bool   `json:"hotStreak"`
+		Wins         int    `json:"wins"`
+		Veteran      bool   `json:"veteran"`
+		Losses       int    `json:"losses"`
+		Rank         string `json:"rank"`
+		Inactive     bool   `json:"inactive"`
+		FreshBlood   bool   `json:"freshBlood"`
+		SummonerID   string `json:"summonerId"`
+		LeaguePoints int    `json:"leaguePoints"`
+	} `json:"entries"`
+	Queue string `json:"queue"`
+	Name  string `json:"name"`
+}
+
+// LeagueEntryDTO represents league entry.
+type LeagueEntryDTO struct {
+	QueueType    string `json:"queueType"`
+	SummonerName string `json:"summonerName"`
+	HotStreak    bool   `json:"hotStreak"`
+	Wins         int    `json:"wins"`
+	Veteran      bool   `json:"veteran"`
+	Losses       int    `json:"losses"`
+	Rank         string `json:"rank"`
+	Tier         string `json:"tier"`
+	Inactive     bool   `json:"inactive"`
+	FreshBlood   bool   `json:"freshBlood"`
+	LeagueID     string `json:"leagueId"`
+	SummonerID   string `json:"summonerId"`
+	LeaguePoints int    `json:"leaguePoints"`
+}
+
+//GetChallengerLeaguesByQueue is a function returns CurrentGameInfo.
+func (me *Client) GetChallengerLeaguesByQueue(q QueueType) (LeagueListDTO, error) {
+	data, networkError := getRequest(me.EndPoint+"/lol/league/v4/challengerleagues/by-queue/", me.Key, q.String())
+	var leagueListDTO LeagueListDTO
+	if networkError == nil {
+		if decodeError := json.Unmarshal(data, &leagueListDTO); decodeError != nil {
+			return leagueListDTO, decodeError
+		}
+		return leagueListDTO, nil
+	}
+	return leagueListDTO, networkError
+}
+
+//GetGrandmasterLeaguesByQueue is a function returns CurrentGameInfo.
+func (me *Client) GetGrandmasterLeaguesByQueue(q QueueType) (LeagueListDTO, error) {
+	data, networkError := getRequest(me.EndPoint+"/lol/league/v4/grandmasterleagues/by-queue/", me.Key, q.String())
+	var leagueListDTO LeagueListDTO
+	if networkError == nil {
+		if decodeError := json.Unmarshal(data, &leagueListDTO); decodeError != nil {
+			return leagueListDTO, decodeError
+		}
+		return leagueListDTO, nil
+	}
+	return leagueListDTO, networkError
+}
+
+//GetMasterLeaguesByQueue is a function returns CurrentGameInfo.
+func (me *Client) GetMasterLeaguesByQueue(q QueueType) (LeagueListDTO, error) {
+	data, networkError := getRequest(me.EndPoint+"/lol/league/v4/masterleagues/by-queue/", me.Key, q.String())
+	var leagueListDTO LeagueListDTO
+	if networkError == nil {
+		if decodeError := json.Unmarshal(data, &leagueListDTO); decodeError != nil {
+			return leagueListDTO, decodeError
+		}
+		return leagueListDTO, nil
+	}
+	return leagueListDTO, networkError
+}
+
+//GetLeaguesByQueue is a function returns CurrentGameInfo.
+//TO DO: Optional
+func (me *Client) GetLeaguesByQueue(q QueueType, tier Tier, division int) ([]LeagueEntryDTO, error) {
+	data, networkError := getRequest(me.EndPoint+"/lol/league/v4/entries/" + q.String() + "/" + tier.String() + "/" + toRoma(division), me.Key, "")
+	var leagueEntryDTO []LeagueEntryDTO
+	if networkError == nil {
+		if decodeError := json.Unmarshal(data, &leagueEntryDTO); decodeError != nil {
+			return leagueEntryDTO, decodeError
+		}
+		return leagueEntryDTO, nil
+	}
+	return leagueEntryDTO, networkError
+}
+
+func toRoma(division int) string {
+	switch division {
+	case 1:
+		return "I"
+	case 2:
+		return "II"
+	case 3:
+		return "III"
+	case 4:
+		return "IV"
+	default:
+		return "Unknown"
+	}
+}
+
+//GetLeaguesByLeagueID is a function returns CurrentGameInfo.
+func (me *Client) GetLeaguesByLeagueID(leagueID string) (LeagueListDTO, error) {
+	data, networkError := getRequest(me.EndPoint+"/lol/league/v4/leagues/", me.Key, leagueID)
+	var leagueListDTO LeagueListDTO
+	if networkError == nil {
+		if decodeError := json.Unmarshal(data, &leagueListDTO); decodeError != nil {
+			return leagueListDTO, decodeError
+		}
+		return leagueListDTO, nil
+	}
+	return leagueListDTO, networkError
+}
+
+//GetEntriesbySummoner is a function returns league entries in all queues for a given summoner ID.
+func (me *Client) GetEntriesbySummoner(encryptedSummonerID string) ([]LeagueEntryDTO, error) {
+	data, networkError := getRequest(me.EndPoint+"/lol/league/v4/entries/by-summoner/", me.Key, encryptedSummonerID)
+	var leagueEntryDTO []LeagueEntryDTO
+	if networkError == nil {
+		if decodeError := json.Unmarshal(data, &leagueEntryDTO); decodeError != nil {
+			return leagueEntryDTO, decodeError
+		}
+		return leagueEntryDTO, nil
+	}
+	return leagueEntryDTO, networkError
+}
--- src/RiotAPI.go
+++ src/RiotAPI.go
@@ -13,6 +13,68 @@ var httpclient http.Client
 var client *Client
 var endPoint string
 
+// QueueType has consts RANKEDSOLO5x5, RANKEDFLEXSR and RANKEDFLEXTT.
+type QueueType int
+const (
+	_ QueueType = iota
+	// RANKEDSOLO5x5 represents solo queue
+	RANKEDSOLO5x5
+	// RANKEDFLEXSR represents flex queue
+	RANKEDFLEXSR
+	// RANKEDFLEXTT represents flex queue
+    RANKEDFLEXTT
+)
+
+func (q QueueType) String() string {
+    switch q {
+    case RANKEDSOLO5x5:
+        return "RANKED_SOLO_5x5"
+    case RANKEDFLEXSR:
+        return "RANKED_FLEX_SR"
+    case RANKEDFLEXTT:
+        return "RANKED_FLEX_TT"
+    default:
+        return "Unknown"
+    }
+}
+
+// Tier has consts IRON to DIAMOND.
+type Tier int
+const (
+	_ Tier = iota
+	// DIAMOND tier
+	DIAMOND
+	// PLATINUM tier
+	PLATINUM
+	// GOLD tier
+	GOLD
+	// SILVER tier
+	SILVER
+	// BRONZE tier
+	BRONZE
+	// IRON tier
+	IRON
+)
+
+func (q Tier) String() string {
+    switch q {
+    case DIAMOND:
+        return "DIAMOND"
+    case PLATINUM:
+        return "PLATINUM"
+    case GOLD:
+		return "GOLD"
+	case SILVER:
+		return "SILVER"
+	case BRONZE:
+		return "BRONZE"
+	case IRON:
+		return "IRON"
+    default:
+        return "Unknown"
+    }
+}
+
 //Client is for RiotAPI
 type Client struct {
 	EndPoint string
